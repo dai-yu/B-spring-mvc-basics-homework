a18991b54e178ef0396fd267f76469909ba14c00
dai-yu <1435480344@qq.com>
2020-11-05 12:29:29 +0800

    [daiyu] register and did not determine whether the user exists

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/UserController.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.capacity.gtb.mvc.controller;
 
 import com.thoughtworks.capacity.gtb.mvc.domain.User;
+import com.thoughtworks.capacity.gtb.mvc.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,8 +13,12 @@ import javax.validation.Valid;
 @RestController
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody @Valid User user){
+        userService.register(user);
         return ResponseEntity.created(null).build();
     }
 
--- src/main/java/com/thoughtworks/capacity/gtb/mvc/service/UserService.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/service/UserService.java
@@ -1,5 +1,26 @@
 package com.thoughtworks.capacity.gtb.mvc.service;
 
+import com.thoughtworks.capacity.gtb.mvc.domain.User;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class UserService {
+    private Map<String, User> userMap = new HashMap<>();
+
+
+    public void register(User user) {
+        if (isExist(user)) {
+
+        }
+        userMap.put(user.getUsername(),user);
+    }
+
+    private boolean isExist(User user) {
+        return userMap.containsKey(user.getUsername());
+    }
+
 
 }

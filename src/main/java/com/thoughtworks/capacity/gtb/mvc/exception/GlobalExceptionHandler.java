8b61f84a167e7b10e75abce73a68bb9a8a532b18
dai-yu <1435480344@qq.com>
2020-11-05 15:04:08 +0800

    [daiyu] validate User

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/exception/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/exception/GlobalExceptionHandler.java
@@ -2,8 +2,12 @@ package com.thoughtworks.capacity.gtb.mvc.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,5 +16,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorMessage> handle(UserNameExistException ex) {
         ErrorMessage errorMessage = new ErrorMessage("400",ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorMessage>> handle(MethodArgumentNotValidException ex) {
+        List<ErrorMessage> errorMessages=new LinkedList<>();
+        ex.getBindingResult().getFieldErrors().forEach(fieldError -> errorMessages.add(new ErrorMessage("400",fieldError.getDefaultMessage())));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessages);
     }
 }
